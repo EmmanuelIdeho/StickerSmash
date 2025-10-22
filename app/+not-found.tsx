0dc8726a8d5fe48217f8dcfabf5f62da76e82367
EmmanuelIdeho <ohis98@gmail.com>
2025-10-22 00:34:18 -0400

    Fallback Not Found screen

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+
+
+export default function NotFoundScreen(){
+    const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
+    return(
+    <>
+        <Stack.Screen options={{title:"Oops! There's nothing here :("}} />
+        <View style={styles.container}>
+            <Link href="/" style={styles.button}>Go back to Home!</Link>
+        </View>
+    </>
+    ) 
+   
+}
